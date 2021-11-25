48f9cd9f3931a6d5371351d3cc1b9c04324445af
tamashun9 <tamashun9@gmail.com>
2021-11-26 04:03:05 +0900

    RonriSakuzyo

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -14,7 +14,7 @@ class PostController extends Controller
     
     public function show(Post $post)
     {
-    return view('posts/show')->with(['post' => $post]);
+        return view('posts/show')->with(['post' => $post]);
     }
     
     public function create()
@@ -29,5 +29,24 @@ class PostController extends Controller
         return redirect('/posts/' . $post->id);
     }
     
+    public function edit(Post $post)
+    {
+        return view('posts/edit')->with(['post' => $post]);
+    }
+    
+    public function update(PostRequest $request, Post $post)
+    {
+        $input_post = $request['post'];
+        $post->fill($input_post)->save();
+
+        return redirect('/posts/' . $post->id);
+    }
+    
+   public function delete(Post $post)
+    {   
+        $post->delete();
+        return redirect('/');
+    }
 }
 ?>
+
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -12,6 +12,14 @@
         <h1 class="title">
             {{ $post->title }}
         </h1>
+           <p class="edit">[<a href="/posts/{{ $post->id }}/edit">edit</a>]</p>
+          <form action="/posts/{{ $post->id }}" id="form_{{ $post->id }}"method="post" style="display:inline">
+                {{ csrf_field() }}
+                {{ method_field('DELETE') }}
+                <input type="submit" style="display:none"> 
+                <p class="delete">[<span onclick="return deletePost(this);">delete</span>]</p>
+            </form>
+         
         <div class="content">
             <div class="content__post">
                 <h3>本文</h3>
@@ -22,7 +30,18 @@
         <div class="footer">
             <a href="/">戻る</a>
         </div>
-       
-        
     </body>
+    
+     <script>
+            function deletePost(e) {
+                'use strict';
+                if(confirm('本当に削除しますか？')) {
+                    document.getElementById("form_{{ $post->id }}").submit();
+                }
+                
+            }
+    </script>   
+    
+    
+    
 </html>
